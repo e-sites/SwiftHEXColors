aaeb4b415b578ed81db3da32fb57cd9c322fa70a
Bas van Kuijck <bas@e-sites.nl>
2019-05-03 13:32:28 +0200

    Package.swift

--- Package.swift
+++ Package.swift
@@ -1,3 +1,18 @@
+// swift-tools-version:5.0
 import PackageDescription
 
-let package = Package(name: "SwiftHEXColors")
+let package = Package(
+    name: SwiftHEXColors,
+    products: [
+        .library(name: "SwiftHEXColors", targets: ["SwiftHEXColors"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "SwiftHEXColors",
+            dependencies: [  ],
+            path: "Sources"
+        )
+    ]
+)
